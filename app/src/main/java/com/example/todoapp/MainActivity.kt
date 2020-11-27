d5f813d7c9354cd79c4fb996094ed9a8b888b250
kamishi <66789856+yasa2028@users.noreply.github.com>
2020-11-27 23:03:07 +0900

    リスト表示の処理を追加
    Signed-off-by: Yosh1aki <kamishioiri1016@gmail.com>

--- app/src/main/java/com/example/todoapp/MainActivity.kt
+++ app/src/main/java/com/example/todoapp/MainActivity.kt
@@ -1,11 +1,13 @@
 package com.example.todoapp
 
 import android.os.Bundle
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
-import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
 
@@ -16,8 +18,22 @@ class MainActivity : AppCompatActivity() {
 
         findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
+                .setAction("Action", null).show()
         }
+        // 実際にアクティビティに表示する配列
+        val listValues: Array<String> = arrayOf("red", "blue", "green")
+        var listView: ListView = ListView(this)
+        setContentView(listView)
+
+        // Adapter：データとListViewを結びつける要素
+        val adapter: ArrayAdapter<String> = ArrayAdapter(
+            this,
+            android.R.layout.simple_list_item_1,
+            listValues
+        )
+
+        // adapter を listView に紐づける。
+        listView.adapter = adapter
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
